fa20beaec9d47ca34df96af505a193815bbb95c6
tino <tino@tino-desktop>
2015-04-05 15:34:14 +0200

    dynamic mobile menu added

---
+++ freunde-fetzen/index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>freunde-fetzen.de</title>
+        
+      
+       <link rel="stylesheet" href="rmm-css/responsivemobilemenu.css" type="text/css"/>
+        <script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
+        <script type="text/javascript" src="rmm-js/responsivemobilemenu.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+    </head>
+    <body>
+        <div class="rmm">
+        <ul>
+            <li><a href='#home'>Home</a></li>
+            <li><a href='#about-me'>Forum</a></li>
+            <li><a href='#gallery'>Galerie</a></li>
+            <li><a href='#blog'>FM</a></li>
+            <li><a href='#links'>Upload</a></li>
+            <li><a href='#sitemap'>Admin</a></li> 
+       </ul>
+        </div>
+        <div>
+            Test test test</br>
+        Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>
+              Test test test</br>
+        Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>      Test test test</br>
+        Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>      Test test test</br>
+        Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>      Test test test</br>
+        Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>      Test test test</br>
+        Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>      Test test test</br>
+        Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>      Test test test</br>
+        Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>      Test test test</br>
+        Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>      Test test test</br>
+        Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>      Test test test</br>
+        Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>      Test test test</br>
+        Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>      Test test test</br>
+        Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>      Test test test</br>
+        Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>      Test test test</br>
+        Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>      Test test test</br>
+        Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>      Test test test</br>
+        Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>Test test test</br>
+        Test test test</br>Test test test</br>Test test test</br>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        </div>
+        
+        
+        
+        
+        
+        
+        
+        
+    </body>
+</html>
