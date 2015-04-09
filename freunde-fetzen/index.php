f42f61e541499bcbcb3c7f39cda271f59244943c
tino <tino@tino-desktop>
2015-04-09 23:35:39 +0200

    header und footer jetzt auf 100%, bootstrap includiert, header bei mobil deaktiviert, Menu an neuen header angepasst.

--- freunde-fetzen/index.php
+++ freunde-fetzen/index.php
@@ -4,16 +4,21 @@
     <head>
         <meta charset="UTF-8">
         <title>freunde-fetzen.de</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <link rel="stylesheet" href="css/layout.css" type="text/css"/>
         <link rel="stylesheet" href="css/responsivemobilemenu.css" type="text/css"/>
         <script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
         <script type="text/javascript" src="js/responsivemobilemenu.js"></script>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        
+        
     </head>
     <body>
         
-        <header>Freunde-Fetzen.de</header>
-        <nav class="rmm">
+        <header class="hidden-xs"><h1>Freunde-Fetzen.de</h1></header>
+        <nav class="rmm" data-menu-title = "Freunde-Fetzen.de">
         <ul>
             <li><a href='#home'>Home</a></li>
             <li><a href='#about-me'>Forum</a></li>
@@ -24,11 +29,13 @@
        </ul>
         </nav>
         
-        <div id="user_panel" style="display: hidden;">
-            <form id="login_form">
-                <input class="login" autofocus required type="text" name="user" placeholder="Name">
-                <input class="login" required type="password" name="password" placeholder="Passwort">
-                <input type="submit" value="Login">
+        <div id="user_panel">
+            <form id="login_form" class="form-inline login_form">
+                
+                <input class="form-control login" autofocus required type="text" name="user" placeholder="Name">
+                <input class="form-control login" required type="password" name="password" placeholder="Passwort">
+                <button type="submit" class="btn btn-default">Login</button>
+                
             </form>
         </div>
         
@@ -71,8 +78,7 @@
         <footer>Freunde-Fetzen.de <?php echo date("Y");?> &copy; T-No</footer>
         
         
-        
-        
+
         
         
     </body>
