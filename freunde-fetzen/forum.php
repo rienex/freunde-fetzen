3f1985fe4830cbd701f9fe47b15ac53eac39f6d5
tino <tino@tino-desktop>
2015-05-16 15:31:27 +0200

    forum layout weiter bearbeitet

--- freunde-fetzen/forum.php
+++ freunde-fetzen/forum.php
@@ -1,98 +1,159 @@
 <?php include 'layout_top.php'; ?>
+<div class="container_post">
 
-<div id="forum_menu" class=" hidden-xs">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title">Events</h3>
-        </div>
-                <div class="panel-body">
-                    <a href="#"><p class="c">RAR 2015</p></a>
-                    <p class="c">Deinchbrand</p>
-                    <p class="c">highfield 15</p>
-                  <p class="c">tino toll party</p>
+    <div class="post_cell">
+        <div class="post">
+            <div class="panel panel-default">
+                <div class="panel-heading visible-xs-block">
+                    <div class="container_post">
+                        <div class="post_cell" style="width:60px;"><img src="avatar/DSCF3198.jpg" width="50px;" alt=""/></div>
+                        <div class="post_cell visible-xs-block"><b>T-No</b> <br>16.04.15 - 20:45 uhr <br> zuletzt bearbeitet: 16.04.15 - 21:35</div>
+                    </div>   
                 </div>
-         <div class="panel-heading">
-            <h3 class="panel-title">Partys</h3>
+                <div class="container_post">
+                    <div class="post_avatar hidden-xs"><img src="avatar/DSCF3198.jpg" width="100px;" alt=""/></div>
+                    <div class="panel-body post_beitrag">
+                       hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                          hier ist der beitrag von RAR 2015
+                           hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                          hier ist der beitrag von RAR 2015
+                           hier ist der beitrag von RAR 2015
+                    </div>
+                </div>
+                <div class="panel-footer">
+                    <div class="container_post ">
+                        <div class="post_cell hidden-xs"><span><b>T-No</b> - 16.04.15 - 20:45 uhr <br> zuletzt bearbeitet: 16.04.15 - 21:35</span> </div>
+                        <div class="post_cell antwort_button hidden-xs"><input class="btn btn-default" type="button" value="antworten"></div>
+                        <input class="btn btn-default visible-xs-block " type="button" value="antworten">
+                    </div>
+                </div>
+          </div>
         </div>
-                <div class="panel-body">
+
+
+        <div class="post">
+            <div class="panel panel-default">
+                <div class="panel-heading visible-xs-block">
+                    <div class="container_post">
+                        <div class="post_cell" style="width:55px;"><img src="avatar/DSCF3198.jpg" width="50px;" alt=""/></div>
+                        <div class="post_cell visible-xs-block"><b>T-No</b> <br>16.04.15 - 20:45 uhr <br> zuletzt bearbeitet: 16.04.15 - 21:35</div>
+                    </div>   
+                </div>
+                <div class="container_post">
+                    <div class="post_avatar hidden-xs"><img src="avatar/DSCF3198.jpg" width="100px;" alt=""/></div>
+                    <div class="panel-body post_beitrag">
+                       hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                          hier ist der beitrag von RAR 2015
+                           hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                          hier ist der beitrag von RAR 2015
+                           hier ist der beitrag von RAR 2015
+                    </div>
+                </div>
+                <div class="panel-footer">
+                    <div class="container_post ">
+                        <div class="post_cell hidden-xs"><span><b>T-No</b> - 16.04.15 - 20:45 uhr <br> zuletzt bearbeitet: 16.04.15 - 21:35</span> </div>
+                        <div class="post_cell antwort_button hidden-xs"><input class="btn btn-default" type="button" value="antworten"></div>
+                        <input class="btn btn-default visible-xs-block " type="button" value="antworten">
+                    </div>
+                </div>
+          </div>
+        </div>
+
+        <div class="post">
+            <div class="panel panel-default">
+                <div class="panel-heading visible-xs-block">
+                    <div class="container_post">
+                        <div class="post_cell" style="width:55px;"><img src="avatar/DSCF3198.jpg" width="50px;" alt=""/></div>
+                        <div class="post_cell visible-xs-block"><b>T-No</b> <br>16.04.15 - 20:45 uhr <br> zuletzt bearbeitet: 16.04.15 - 21:35</div>
+                    </div>   
+                </div>
+                <div class="container_post">
+                    <div class="post_avatar hidden-xs"><img src="avatar/DSCF3198.jpg" width="100px;" alt=""/></div>
+                    <div class="panel-body post_beitrag">
+                       hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                          hier ist der beitrag von RAR 2015
+                           hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                      hier ist der beitrag von RAR 2015
+                          hier ist der beitrag von RAR 2015
+                           hier ist der beitrag von RAR 2015
+                    </div>
+                </div>
+                <div class="panel-footer">
+                    <div class="container_post ">
+                        <div class="post_cell hidden-xs"><span><b>T-No</b> - 16.04.15 - 20:45 uhr <br> zuletzt bearbeitet: 16.04.15 - 21:35</span> </div>
+                        <div class="post_cell antwort_button hidden-xs"><input class="btn btn-default" type="button" value="antworten"></div>
+                        <input class="btn btn-default visible-xs-block " type="button" value="antworten">
+                    </div>
+                </div>
+          </div>
+        </div>
+
+    </div>
+    <div class="post_cell_margin">
+        <div id="forum_menu " class="hidden-xs post_cell">
+            <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">Events</h3>
+            </div>
+            <div class="panel-body">
+                <a href="#"><p class="c">RAR 2015</p></a>
+                <a href="#"><p class="c">Deinchbrand</p></a>
+                <a href="#"><p class="c">highfield 15</p></a>
+                <a href="#"><p class="c">tino toll party</p></a>
+            </div>
+            <div class="panel-heading">
+               <h3 class="panel-title">Partys</h3>
+           </div>
+            <div class="panel-body">
                 <table class="table table-hover">
-                    <tr><td>Silvester</td></tr>  
-                    <tr><td>Silvester</td></tr>
-                    <tr><td>Silvester</td></tr>
-                    <tr><td>Silvester</td></tr>
+                    <a href="#"><p class="c">Silvester</p></a>
+                    <a href="#"><p class="c">Geburtstag</p></a>
+                    <a href="#"><p class="c">tino toll party</p></a>
                 </table>
-                </div>
+            </div>
+        </div>
+        </div>
     </div>
 </div>
-
-<div class="post">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title"><b>Rock am Ring 2015</b></h3>
-        </div>
-        <div class="panel-body">
-           hier ist der beitrag von RAR 2015
-          hier ist der beitrag von RAR 2015
-          hier ist der beitrag von RAR 2015
-          hier ist der beitrag von RAR 2015
-          hier ist der beitrag von RAR 2015
-          hier ist der beitrag von RAR 2015
-          hier ist der beitrag von RAR 2015
-          hier ist der beitrag von RAR 2015
-        </div>
-         <div class="panel-footer">T-No - 16.04.15</div>
-  </div>
-</div>
-
-<div class="post">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title"><b>Rock am Ring 2015</b></h3>
-        </div>
-        <div class="panel-body">
-          hier ist der beitrag von RAR 2015
-        </div>
-         <div class="panel-footer">T-No - 16.04.15</div>
-  </div>
-</div>
-
-<div class="post">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title"><b>Rock am Ring 2015</b></h3>
-        </div>
-        <div class="panel-body">
-          hier ist der beitrag von RAR 2015
-          hier ist der beitrag von RAR 2015
-          hier ist der beitrag von RAR 2015
-          hier ist der beitrag von RAR 2015
-          hier ist der beitrag von RAR 2015
-          hier ist der beitrag von RAR 2015
-          hier ist der beitrag von RAR 2015
-          hier ist der beitrag von RAR 2015
-        </div>
-         <div class="panel-footer">T-No - 16.04.15</div>
-  </div>
-</div>
-
-<div class="post">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title"><b>Rock am Ring 2015</b></h3>
-        </div>
-        <div class="panel-body">
-           hier ist der beitrag von RAR 2015
-          hier ist der beitrag von RAR 2015
-          hier ist der beitrag von RAR 2015
-          hier ist der beitrag von RAR 2015
-          hier ist der beitrag von RAR 2015
-          hier ist der beitrag von RAR 2015
-          hier ist der beitrag von RAR 2015
-          hier ist der beitrag von RAR 2015
-        </div>
-         <div class="panel-footer">T-No - 16.04.15</div>
-  </div>
-</div>
-
-
 <?php include 'layout_bottom.php'; ?>
